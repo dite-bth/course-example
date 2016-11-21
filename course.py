8deb7f676d3a40cd6a7a27163f7ee6b80709618e
mattische <schertell@gmail.com>
2016-11-21 15:21:42 +0100

    added student

--- course.py
+++ course.py
@@ -28,15 +28,3 @@ class Course():
         return s
 
 
-
-s1 = Student("Nils", "nile16", 15)
-s2 = Student("Tommi", "tosv15", 105)
-sList = [s1, s2, Student("Fredrik", "frfr14", 2)]
-
-c = Course("KURSEN", "ME1572", 15, sList)
-
-
-
-c.addStudent(Student("Jesper", "jetr16", 15))
-
-print(c)
