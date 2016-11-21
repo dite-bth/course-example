49db75e1aba7ab46a1cbef17c4c68d4771b12712
mattische <schertell@gmail.com>
2016-11-21 14:35:19 +0100

    kalopps

--- course.py
+++ course.py
@@ -1,1 +1,25 @@
 class Course():
+
+    def __init__(self, name, code, points, students):
+        self.name = name
+        self.code = code
+        self.points = points
+
+        self.studentList = list()
+        self.studentList = students
+        self.studentList.sort()
+
+
+    def addStudent(self, studentObj):
+        self.studentList.append(studentObj)
+        self.studentList.sort()
+
+
+    def removeStudent(self, studentObj):
+        self.studentList.remove(studentObj)
+
+    def __str__(self):
+        s = self.name + "\n" + self.code + " " + self.points + "HP\n"
+
+        for student in self.studentList:
+            s += student + "\n"
