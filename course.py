406780aae6260a8756eed0e4af114c5a74305e08
mattische <schertell@gmail.com>
2016-11-22 09:39:53 +0100

    work in progress...

--- course.py
+++ course.py
@@ -2,28 +2,19 @@ from student import Student
 
 class Course():
 
-    def __init__(self, name, code, points, students):
+    def __init__(self, name, code, points):
         self.name = name
         self.code = code
         self.points = points
 
-        self.studentList = list()
-        self.studentList = students
 
 
-    def addStudent(self, studentObj):
-        self.studentList.append(studentObj)
 
 
-    def removeStudent(self, studentObj):
-        self.studentList.remove(studentObj)
 
 
     def __str__(self):
-        s = self.name + "\n" + self.code + " " + str(self.points) + "HP\n"
-
-        for student in self.studentList:
-            s += str(student) + "\n"
+        s = self.name + "\n" + self.code + " " + str(self.points) + "HP"
 
         return s
 
--- main.py
+++ main.py
@@ -7,15 +7,15 @@ s1 = Student("Nils", "nile16", 15)
 s2 = Student("Tommi", "tosv15", 105)
 sList = [s1, s2, Student("Fredrik", "frfr14", 2)]
 
-c = Course("KURSEN", "ME1572", 15, sList)
+c = Course("KURSEN", "ME1572", 15)
 
 
-p = Program("Kalops", 180, [c])
+p = Program("Kalops", 180, [c], sList)
 
 
-c.addStudent(Student("Jesper", "jetr16", 15))
+p.addStudent(Student("Jesper", "jetr16", 15))
 
-ss = p.getStudentsInCourses()
+ss = p.getStudents()
 print(ss)
 
 for s in ss:
