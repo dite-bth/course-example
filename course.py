b26f65a24ac3acb12b4fa37322b703ba02092663
mattische <schertell@gmail.com>
2016-11-22 10:01:42 +0100

    work in progress...

--- course.py
+++ course.py
@@ -2,19 +2,19 @@ from student import Student
 
 class Course():
 
-    def __init__(self, name, code, points):
+    def __init__(self, name, code, points, teacherObj=None):
         self.name = name
         self.code = code
         self.points = points
 
-
-
-
-
+        self.courseAdmin = teacherObj
 
 
     def __str__(self):
-        s = self.name + "\n" + self.code + " " + str(self.points) + "HP"
+        s = self.name + "\n" + self.code + " " + str(self.points) + "HP. "
+
+        if not self.courseAdmin == None:
+            s += self.courseAdmin.name
 
         return s
 
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from course import Course
 from student import Student
 from program import Program
-
+from teacher import Teacher
 
 s1 = Student("Nils", "nile16", 15)
 s2 = Student("Tommi", "tosv15", 105)
 sList = [s1, s2, Student("Fredrik", "frfr14", 2)]
 
-c = Course("KURSEN", "ME1572", 15)
+t = Teacher("Hasse", "haha@hej.com")
+
+c = Course("KURSEN", "ME1572", 15, t)
 
 
-p = Program("Kalops", 180, [c], sList)
+p = Program("Kalops", 180, [c], sList, t)
 
 
 p.addStudent(Student("Jesper", "jetr16", 15))
 
 ss = p.getStudents()
-print(ss)
 
-for s in ss:
-    print(s)
+print(c)
+print(p)
