024a36b923a87a88f7a9cdb247a313ac4a994e2d
mattische <schertell@gmail.com>
2016-11-28 11:39:33 +0100

    work in progress...

--- main.py
+++ main.py
@@ -6,32 +6,6 @@ from school_nile16 import School
 
 import sys
 
-HEEEEEEEEEEEEEELP!!!!!!!!!!!
 
-
-
-def inputSchool():
-    n = input("School name: ")
-    c = input("School city: ")
-
-    sc = School(n, c)
-
-def inputStudent():
-    n = input("Enter student name: ")
-
-    a = n.split()
-    print(a)
-    a = ''.join(w[0:2].lower() for w in a)
-    s = Student(n, a)
-
-def inputC
-
-while True:
-    q = input("Select action 1. Student 2. Course 3. Program 4. School 5. Quit: ")
-
-    if q == "5":
-        sys.exit()
-    elif q == "1":
-
-    else:
-        print("Please enter 1 to 5.")
+t = Teacher("Arne", "a@hej.ru")
+print(t)
